f13b1fc4b591f81af71b2a6b3cd204db62c9cdaf
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2020-07-01 01:31:08 +0200

    Revert changes in NMS

--- Bukkit-1.16/src/main/java/se/hyperver/hyperverse/NMSImpl.java
+++ Bukkit-1.16/src/main/java/se/hyperver/hyperverse/NMSImpl.java
@@ -193,6 +193,10 @@ public class NMSImpl implements NMS {
                 entityPlayer.reset();
                 entityPlayer.load(compound);
 
+                // entityPlayer.updateEffects = true;
+                // entityPlayer.updateAbilities();
+                player.teleport(originLocation);
+
                 final WorldServer worldServer = ((CraftWorld) originLocation.getWorld()).getHandle();
                 final DimensionManager dimensionManager = worldServer.getDimensionManager();
 
@@ -200,9 +204,6 @@ public class NMSImpl implements NMS {
                 // Prevent annoying message
                 entityPlayer.decouple();
                 worldServer.removePlayer(entityPlayer);
-                // entityPlayer.updateEffects = true;
-                // entityPlayer.updateAbilities();
-                player.teleport(originLocation);
                 // worldServer.removePlayer above should remove the player from the
                 // map, but that doesn't always happen. This is a last effort
                 // attempt to prevent the annoying "Force re-added" message
