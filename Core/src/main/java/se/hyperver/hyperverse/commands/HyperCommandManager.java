ea65b01a8027b4f0b8e23bf697c4f2cefeaf4e01
Fefo6644 <federico.lopez.1999@outlook.com>
2020-08-07 18:34:51 -0300

    Things * Added a missing '@' to the command description annotation of `command.teleportgroup`. * Added `HyperWorld#unloadWorld(boolean)` and `SimpleWorld#unloadWorld(boolean)`, the parameter indicates whether the world should be saved to disk when unloading. * Now `SimpleWorld#unloadWorld()` calls `SimpleWorld#unloadWorld(boolean)` passing `true` as argument. * Added optional boolean argument for `/hv unload <world> [true|false]` to whether the world should be saved to disk when unloading. Defaults to true.

--- Core/src/main/java/se/hyperver/hyperverse/commands/HyperCommandManager.java
+++ Core/src/main/java/se/hyperver/hyperverse/commands/HyperCommandManager.java
@@ -579,7 +579,7 @@ public class HyperCommandManager extends BaseCommand {
 
     @Category("Misc") @Subcommand("teleportgroup|tpgroup") @CommandAlias("hvtpgroup")
     @CommandPermission("hyperverse.teleportgroup")
-    @CommandCompletion("@profile_groups:has_perms=true") @Description("{@command.teleportgroup}")
+    @CommandCompletion("@profile_groups:has_perms=true") @Description("{@@command.teleportgroup}")
     public void doGroupedTeleport(final Player sender, final String profileGroup) {
         final CompletableFuture<Collection<PersistentLocation>> future =
             (CompletableFuture<Collection<PersistentLocation>>) Hyperverse
@@ -619,8 +619,9 @@ public class HyperCommandManager extends BaseCommand {
     }
 
     @Category("Management") @Subcommand("unload") @CommandPermission("hyperverse.unload")
-    @CommandCompletion("@hyperworlds:state=loaded") @Description("{@@command.unload}")
-    public void doUnload(final CommandSender sender, final HyperWorld world) {
+    @CommandCompletion("@hyperworlds:state=loaded true|false") @Description("{@@command.unload}")
+    public void doUnload(final CommandSender sender, final HyperWorld world,
+                         @Default("true") final boolean saveWorld) {
         if (world == null) {
             return;
         }
@@ -628,7 +629,7 @@ public class HyperCommandManager extends BaseCommand {
             MessageUtil.sendMessage(sender, Messages.messageWorldNotLoaded);
             return;
         }
-        final HyperWorld.WorldUnloadResult worldUnloadResult = world.unloadWorld();
+        final HyperWorld.WorldUnloadResult worldUnloadResult = world.unloadWorld(saveWorld);
         if (worldUnloadResult == HyperWorld.WorldUnloadResult.SUCCESS) {
             MessageUtil.sendMessage(sender, Messages.messageWorldUnloaded);
         } else {
